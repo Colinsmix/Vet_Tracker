e0d5380a391d6fad307711586a6decb6745ec63a
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-23 18:38:59 -0500

    Created Models for Pet, User, Type, Breed

--- db/migrate/20131223233544_create_types.rb
+++ db/migrate/20131223233544_create_types.rb
@@ -1,6 +1,7 @@
 class CreateTypes < ActiveRecord::Migration
   def change
     create_table :types do |t|
+      t.string :name, null: false
 
       t.timestamps
     end
--- db/migrate/20131223233550_create_breeds.rb
+++ db/migrate/20131223233550_create_breeds.rb
@@ -1,6 +1,8 @@
 class CreateBreeds < ActiveRecord::Migration
   def change
     create_table :breeds do |t|
+      t.string :name, null: false
+      t.integer :type_id, null: false
 
       t.timestamps
     end
